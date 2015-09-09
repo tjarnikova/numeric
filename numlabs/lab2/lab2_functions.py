db85197cfd51cdcf69e820487c2a294977654e31
Phil Austin <paustin@eos.ubc.ca>
2015-09-09 13:57:24 -0700

    new oscillator module

--- numlabs/lab2/lab2_functions.py
+++ numlabs/lab2/lab2_functions.py
@@ -93,6 +93,23 @@ def leapfrog (npts,tend,To,Ta,theLambda):
                             heat(theTemp[timeStep-1],Ta,theLambda)*2.*dt
     return (theTime,theTemp)
 
+def midpoint (npts,tend,To,Ta,theLambda):
+    dt=tend/npts
+    theTemp=np.empty([npts,],np.float64)
+    theTemp[0]=To
+    theTime=np.empty_like(theTemp)
+# estimate first step by forward euler as need two steps to do leapfrog
+    theTemp[1] = To + heat(To,Ta,theLambda)*dt
+    theTime[1] = dt
+# correct first step by estimating the temperature at a half-step
+    Th = To + 0.5*(theTemp[1]-To)
+    theTemp[1] = To + heat(Th,Ta,theLambda)*dt
+    for timeStep in np.arange(2,npts):
+        theTime[timeStep]=theTime[timeStep-1] + dt
+        theTemp[timeStep] = theTemp[timeStep-2]+\
+                            heat(theTemp[timeStep-1],Ta,theLambda)*2.*dt
+    return (theTime,theTemp)
+
 
 def runge(npts,tend,To,Ta,theLambda):
     dt=tend/npts
