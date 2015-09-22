ae9071a13f3f4d40457e2b55405845cd0e8d6a3d
tjarnikova <tjarnik@gmail.com>
2015-09-22 12:32:28 -0700

    d:wq

--- numlabs/lab2/terror2.py
+++ numlabs/lab2/terror2.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from lab2_functions import euler,leapfrog,runge
 import numpy as np
+import os, fnmatch
 
 theFuncs={'euler':euler,'leapfrog':leapfrog,'runge':runge}
 
@@ -9,7 +10,9 @@ if __name__=="__main__":
     To= 30
     tend = 30.0
     theLambda=-0.8
-    npts=100.
+    npts=500.
+    points = str(npts)
+    print(points)
     funChoice='runge'
 
     plt.close('all')
@@ -25,12 +28,13 @@ if __name__=="__main__":
     fig1.clf()
     # Set x limits
     plt.xlim(0,30)
-    plt.ylim(-500,500)
+    plt.ylim(-20,60)
     plt.plot(approxTime_runge,approxTemp_runge, 'b', label="runge")
     plt.plot(approxTime_euler,approxTemp_euler, 'g', label = "euler")
     plt.plot(approxTime_leapfrog,approxTemp_leapfrog, 'r', label = "leapfrog")
     plt.plot()
-    plt.title('Comparison of estimates of a function by 3 approximation methods. Steps: 10')
+    titl =  "Comparison of estimates by 3 approximation methods. Steps: " + points 
+    plt.title(titl)
     plt.hold(True)
     exactTime=np.empty([npts,],np.float)
     exactTemp=np.empty_like(exactTime)
@@ -39,7 +43,14 @@ if __name__=="__main__":
         exactTemp[i] = Ta + (To-Ta)*np.exp(theLambda*exactTime[i])
     plt.plot(exactTime,exactTemp,'k', label = "exact solution")
 
-    plt.legend( loc='lower left' )
+    plt.legend( loc='upper left' )
+
+    figname = "Comparison of estimates - no pts " + points  + ".png"
+
+    filepath = '/Users/alena/Sync/numeric/lab2/asst'
+    figname = os.path.join(filepath, figname)
+    plt.savefig(figname)
+
 
     #(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
      #      ncol=2, mode="expand", borderaxespad=0.)
@@ -51,11 +62,18 @@ if __name__=="__main__":
     difference_euler = approxTemp_euler-exactTemp;
     difference_leapfrog = approxTemp_leapfrog - exactTemp;
     plt.xlim(0,30)
-    plt.ylim(-500,500)
+    plt.ylim(-50,50)
     plt.plot(exactTime,difference_runge, 'b', label= "difference - runge")
     plt.plot(exactTime,difference_euler, 'g', label= "difference - euler")
     plt.plot(exactTime,difference_leapfrog, 'r', label= "difference - leapfrog")
-    plt.title('Difference between approximations and exact solution. Steps: 10')
-    plt.legend( loc='lower left' )
+    titl = "Difference between approximations and exact solution. Steps: " + points
+    plt.title(titl)
+    plt.legend( loc='upper left' )
     plt.hold(True)
+    figname = "Difference between approximations and soln - no pts " + points  + ".png"
+
+    filepath = '/Users/alena/Sync/numeric/lab2/asst'
+    figname = os.path.join(filepath, figname)
+    plt.savefig(figname)
+
     plt.show()
