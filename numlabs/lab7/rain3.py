555f4f99d5414e939c0eb75c27cfa2c0427ae464
tjarnikova <tjarnik@gmail.com>
2015-10-26 17:08:17 -0700

    Robert gets grids more than I do

--- numlabs/lab7/rain3.py
+++ numlabs/lab7/rain3.py
@@ -93,51 +93,50 @@ class Quantity(object):
         self.now = copy.copy(self.next)
 
 
-def initial_conditions(u, h, ho):
+def initial_conditions(u, h, v, ho):
     """Set the initial condition values.
     """
     u.prev[:] = 0
     h.prev[:] = 0
     h.prev[len(h.prev) // 2] = ho
+    v.prev[:] = 0
 
 
-def boundary_conditions(u_array, h_array, n_grid):
+def boundary_conditions(u_array, h_array, v_array, n_grid):
     """Set the boundary condition values.
     """
     u_array[0] = 0
     u_array[n_grid - 1] = 0
-    h_array[0] = h_array[1]
-    h_array[n_grid-1] = h_array[n_grid-2]
-
-
-def first_time_step(u, h, g, H, dt, dx, ho, gu, gh, n_grid):
+    #h_array[0] = h_array[1]
+    #h_array[n_grid-1] = h_array[n_grid-2]
+    v_array[0] = 0
+    v_array[n_grid-1] = 0
+    
+def first_time_step(u, h, v, g, H, dt, dx, ho, gu, gh, n_grid):
     """Calculate the first time step values from the analytical
     predictor-corrector derived from equations 4.18 and 4.19.
     """
-    u.now[1:n_grid - 1] = 0
-    factor = gu * ho / 2
-    midpoint = n_grid // 2
-    u.now[midpoint - 1] = -factor
-    u.now[midpoint + 1] = factor
-    h.now[1:n_grid - 1] = 0
-    h.now[midpoint] = ho - g * H * ho * dt ** 2 / (4 * dx ** 2)
+    u.now[:] = 0
+    factor = -gu * ho 
+    midpoint = n_grid / 2
+    u.now[midpoint - .5] = factor
+    u.now[midpoint + .5] = -factor
+    h.now[:] = 0
+    h.now[midpoint] = ho - g * H * ho * dt ** 2 / (1 * dx ** 2)
+    v.now[:] = 0 
 
 
-def leap_frog(u, h, gu, gh, n_grid):
+def leap_frog(u, h, v, f, gu, gh, n_grid, dt):
     """Calculate the next time step values using the leap-frog scheme
     derived from equations 4.16 and 4.17.
     """
     for pt in np.arange(1, n_grid - 1):
-        u.next[pt] = u.prev[pt] - gu * (h.now[pt + 1] - h.now[pt - 1])
-        h.next[pt] = h.prev[pt] - gh * (u.now[pt + 1] - u.now[pt - 1])
-#     Alternate vectorized implementation:
-#     u.next[1:n_grid - 1] = (u.prev[1:n_grid - 1]
-#                             - gu * (h.now[2:n_grid] - h.now[:n_grid - 2]))
-#     h.next[1:n_grid - 1] = (h.prev[1:n_grid - 1]
-#                             - gh * (u.now[2:n_grid] - u.now[:n_grid - 2]))
+        u.next[pt] = u.prev[pt] - gu * (h.now[pt + 1] - h.now[pt])*2+f*v.now[pt]*2*dt
+        h.next[pt] = h.prev[pt] - gh * (u.now[pt] - u.now[pt - 1])*2
+        v.next[pt] = v.prev[pt] - f * (u.now[pt])*2*dt
 
 
-def make_graph(u, h, dt, n_time):
+def make_graph(u, h, v, dt, n_time):
     """Create graphs of the model results using matplotlib.
 
     You probably need to run the rain script from within ipython,
@@ -147,7 +146,7 @@ def make_graph(u, h, dt, n_time):
     """
 
     # Create a figure with 2 sub-plots
-    fig, (ax_u, ax_h) = plt.subplots(2,1, figsize=(10,10))
+    fig, (ax_u, ax_h, ax_v) = plt.subplots(3,1, figsize=(10,10))
     
     # Set the figure title, and the axes labels.
     the_title = fig.text(0.25, 0.95, 'Results from t = %.3fs to %.3fs' % (0, dt*n_time))
@@ -168,8 +167,16 @@ def make_graph(u, h, dt, n_time):
     # Do the main plot
     for time in range(0, n_time, interval):
         colorVal = scalarMap.to_rgba(time)
-        ax_u.plot(u.store[:, time], color=colorVal)
-        ax_h.plot(h.store[:, time], color=colorVal)
+        u_X = np.arange(0,len(u.store[:,time]))
+        ax_u.plot(u_X,(u.store[:,time]), color = colorVal)
+        v_X = np.arange(0,len(v.store[:,time]))
+        h_X = np.arange(-.5,len(h.store[:,time])-0.5)
+        ax_v.plot(v_X,v.store[:, time], color=colorVal)
+        ax_h.plot(h_X,h.store[:, time], color=colorVal)
+        
+        ax_u.set_xlim([0,len(u.store[:,time])-1])
+        ax_h.set_xlim([0,len(h.store[:,time])-1])
+        ax_v.set_xlim([0,len(v.store[:,time])-1])
 
     # Add the custom colorbar
     ax2 = fig.add_axes([0.95, 0.05, 0.05, 0.9])
@@ -184,49 +191,55 @@ def rain(args):
     """
     n_time = int(args[0])
     n_grid = int(args[1])
-    dt = float(args[2])
-   
-
-    #     n_time, n_grid = map(int, args)
+#     Alternate implementation:
+#     n_time, n_grid = map(int, args)
 
     # Constants and parameters of the model
     g = 980                     # acceleration due to gravity [cm/s^2]
     H = 1                       # water depth [cm]
-    #dt = 0.03594                # time step [s] (orig is .001)
+    dt = 0.001                  # time step [s]
     dx = 1                      # grid spacing [cm]
     ho = 0.01                   # initial perturbation of surface [cm]
+    f = 0.0001
     gu = g * dt / dx            # first handy constant
     gh = H * dt / dx            # second handy constant
     # Create velocity and surface height objects
     u = Quantity(n_grid, n_time)
     h = Quantity(n_grid, n_time)
+    v = Quantity(n_grid, n_time)
     # Set up initial conditions and store them in the time step
     # results arrays
-    initial_conditions(u, h, ho)
+    initial_conditions(u, h,v, ho)
     u.store_timestep(0, 'prev')
     h.store_timestep(0, 'prev')
+    v.store_timestep(0, 'prev')
     # Calculate the first time step values from the
     # predictor-corrector, apply the boundary conditions, and store
     # the values in the time step results arrays
-    first_time_step(u, h, g, H, dt, dx, ho, gu, gh, n_grid)
-    boundary_conditions(u.now, h.now, n_grid)
+    first_time_step(u, h, v, g, H, dt, dx, ho, gu, gh, n_grid)
+    #first_time_step(u, h, g, H, dt, dx, ho, gu, gh, n_grid)
+    boundary_conditions(u.now, h.now,v.now, n_grid)
     u.store_timestep(1, 'now')
     h.store_timestep(1, 'now')
+    v.store_timestep(1, 'now')
     # Time step loop using leap-frog scheme
     for t in np.arange(2, n_time):
         # Advance the solution and apply the boundary conditions
-        leap_frog(u, h, gu, gh, n_grid)
-        boundary_conditions(u.next, h.next, n_grid)
+        leap_frog(u, h, v, f, gu, gh, n_grid, dt)
+        #leap_frog(u, h, gu, gh, n_grid)
+        boundary_conditions(u.next, h.next, v.next, n_grid)
         # Store the values in the time step results arrays, and shift
         # .now to .prev, and .next to .now in preparation for the next
         # time step
         u.store_timestep(t)
         h.store_timestep(t)
+        v.store_timestep(t)
         u.shift()
         h.shift()
+        v.shift()
 
     # Plot the results as colored graphs
-    make_graph(u, h, dt, n_time)
+    make_graph(u, h,v, dt, n_time)
     return
 
 
