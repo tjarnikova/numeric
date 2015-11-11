3c0b981a187875b1a3c97a005ffe088e3ec09b3f
tjarnikova <tjarnik@gmail.com>
2015-11-11 13:18:22 -0800

    WK_power_variance

---
+++ lab9/ubc_fft_class.py
@@ -0,0 +1,107 @@
+from netCDF4 import Dataset
+import numpy as np
+import math
+from numpy import fft
+from matplotlib import pyplot as plt
+
+
+class ubc_fft:
+
+    def __init__(self, filename, var, scale):
+        """
+           Input filename, var=variable name, 
+           scale= the size of the pixel in km
+
+           Constructer opens the netcdf file, reads the data and
+           saves the twodimensional fft
+        """
+        with Dataset(filename,'r') as fin:
+            data = fin.variables[var][...]
+        data = data - data.mean()
+        if data.shape[0] != data.shape[1]:
+            raise ValueError('expecting square matrix')
+        self.xdim = data.shape[0]     # size of each row of the array
+        self.midpoint = int(math.floor(self.xdim/2))
+        root,suffix = filename.split('.')
+        self.filename = root
+        self.var = var
+        self.scale = float(scale)
+        self.data = data
+        self.fft_data = fft.fft2(self.data)
+    
+    def power_spectrum(self):
+        """
+           calculate the power spectrum for the 2-dimensional field
+        """
+        #
+        # fft_shift moves the zero frequency point to the  middle
+        # of the array  
+        #
+        fft_shift = fft.fftshift(self.fft_data)
+        spectral_dens = fft_shift*np.conjugate(fft_shift)/(self.xdim*self.xdim)
+        spectral_dens = spectral_dens.real
+        #
+        # dimensional wavenumbers for 2dim spectrum  (need only the kx
+        # dimensional since image is square
+        #
+        k_vals = np.arange(0,(self.midpoint))+1
+        k_vals = (k_vals-self.midpoint)/(self.xdim*self.scale)
+        self.spectral_dens=spectral_dens
+        self.k_vals=k_vals
+
+    def annular_avg(self,avg_binwidth):
+        """ 
+         integrate the 2-d power spectrum around a series of rings 
+         of radius kradial and average into a set of 1-dimensional
+         radial bins
+        """
+        #
+        #  define the k axis which is the radius in the 2-d polar version of E
+        #
+        numbins = int(round((math.sqrt(2)*self.xdim/avg_binwidth),0)+1)
+
+        avg_spec = np.zeros(numbins,np.float64)
+        bin_count = np.zeros(numbins,np.float64)
+
+        print("\t- INTEGRATING... ")
+        for i in range(self.xdim):
+            if (i%100) == 0:
+                print("\t\trow: {} completed".format(i))
+            for j in range(self.xdim):
+                kradial = math.sqrt(((i+1)-self.xdim/2)**2+((j+1)-self.xdim/2)**2)
+                bin_num = int(math.floor(kradial/avg_binwidth))
+                avg_spec[bin_num]=avg_spec[bin_num]+ kradial*self.spectral_dens[i,j]
+                bin_count[bin_num]+=1
+
+        for i in range(numbins):
+            if bin_count[i]>0:
+                avg_spec[i]=avg_spec[i]*avg_binwidth/bin_count[i]/(4*(math.pi**2))
+        self.avg_spec=avg_spec
+        #
+        # dimensional wavenumbers for 1-d average spectrum
+        #
+        self.k_bins=np.arange(numbins)+1
+        self.k_bins = self.k_bins[0:self.midpoint]
+        self.avg_spec = self.avg_spec[0:self.midpoint]
+
+        
+    
+    def graph_spectrum(self, kol_slope=-5./3., kol_offset=1., \
+                      title=None):
+        """
+           graph the annular average and compare it to Kolmogorov -5/3
+        """
+        avg_spec=self.avg_spec
+        delta_k = 1./self.scale                # 1./km (1/0.025 for landsat 25 meter pixels)
+        nyquist = delta_k * 0.5
+        knum = self.k_bins * (nyquist/float(len(self.k_bins)))# k = w/(25m)
+        #
+        # draw the -5/3 line through a give spot
+        #
+        kol = kol_offset*(knum**kol_slope)
+        fig,ax=plt.subplots(1,1,figsize=(8,8))
+        ax.loglog(knum,avg_spec,'r-',label='power')
+        ax.loglog(knum,kol,'k-',label="$k^{-5/3}$")
+        ax.set(title=title,xlabel='k (1/km)',ylabel='$E_k$')
+        ax.legend()
+        self.plotax=ax
