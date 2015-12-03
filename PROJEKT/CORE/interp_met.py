68623342bcfb5475c9db18361db5466061b5492d
tjarnikova <tjarnik@gmail.com>
2015-12-03 07:45:44 -0800

    made CORE folder

---
+++ PROJEKT/CORE/interp_met.py
@@ -0,0 +1,595 @@
+import numpy as np
+import yaml
+from collections import namedtuple
+import scipy
+import yaml
+import matplotlib.pyplot as plt
+from scipy import interpolate
+import netCDF4
+from netCDF4 import Dataset
+import numpy as np
+from types import *
+import numpy.ma as ma
+import collections
+import glob, os
+import math
+
+def ice_load():
+	filelist = 'icec.day.mean.2014.v2.nc'
+	with Dataset(filelist) as nc:
+	    #print(nc.variables)
+	    icec=nc.variables['icec'][...]
+	    lat =nc.variables['lat'][...] 
+	    lon = nc.variables['lon'][...]
+	    time = nc.variables['time'][...]
+
+	#dealing with the mask in icec - assuming it's a value of 0
+
+	w = icec[0,0,0]
+	lat[0]
+	le_mask = np.ma.getmask(icec)
+	icec[le_mask]=0
+
+	firstday = (time[0]) -1 ;
+	newtime = time - firstday;
+
+	ArcticCircleLat = 66.562778
+	#a circuitous way to find the arctic circle indices
+	LatCircle = np.where(lat> 66.562778)
+	LatCircle = LatCircle[0]
+	latlen = len(LatCircle)
+	latstart = (LatCircle[0])
+	latend = (LatCircle[latlen-1])
+
+	start_indices = [1,32,60,91,121,152,182,213,244,274,305,335]
+	end_indices = [32,60,91,121,152,182,213,244,274,305,335,365]
+	for i in range (len(start_indices)):
+	    start = start_indices[i]
+	    end = end_indices[i]
+
+	    index = (newtime>=start)*(newtime<end)
+	    where_index = np.where(index)
+	    if i == 0:
+	        Jan = where_index
+	    if i == 1:
+	        Feb = where_index
+	    if i == 2:
+	        Mar = where_index
+	    if i == 3:
+	        April = where_index
+	    if i == 4:
+	        May = where_index
+	    if i == 5:
+	        June = where_index
+	    if i == 6:
+	        July = where_index
+	    if i == 7:
+	        Aug = where_index
+	    if i == 8:
+	        Sep = where_index
+	    if i == 9:
+	        Oct = where_index
+	    if i == 10:
+	        Nov = where_index
+	    if i == 11:
+	        Dec = where_index
+	        
+	        
+	Ice_cover = collections.OrderedDict()
+	Ice_cover['Jan'] = np.mean(icec[Jan,latstart:latend,:])
+	Ice_cover['Feb'] = np.mean(icec[Feb,latstart:latend,:])
+	Ice_cover['Mar'] = np.mean(icec[Mar,latstart:latend,:])
+	Ice_cover['April'] = np.mean(icec[April,latstart:latend,:])
+	Ice_cover['May'] = np.mean(icec[May,latstart:latend,:])
+	Ice_cover['June'] = np.mean(icec[June,latstart:latend,:])
+	Ice_cover['July'] = np.mean(icec[July,latstart:latend,:])
+	Ice_cover['Aug'] = np.mean(icec[Aug,latstart:latend,:])
+	Ice_cover['Sep'] = np.mean(icec[Sep,latstart:latend,:])
+	Ice_cover['Oct'] = np.mean(icec[Oct,latstart:latend,:])
+	Ice_cover['Nov'] = np.mean(icec[Nov,latstart:latend,:])
+	Ice_cover['Dec'] = np.mean(icec[Dec,latstart:latend,:])
+
+
+	dataMap = Ice_cover
+
+	starts = [1,32,60,91,121,152,182,213,244,274,305,335]
+	ends = np.zeros(len(starts))
+	mids = np.zeros(len(starts))
+	icevals = np.zeros(len(starts))
+
+
+
+	for i in range(0,12):
+
+		if i == 11:
+			ends[i] = 365 
+			mids[i] = int((starts[i] + ends[i])/2)
+		else:
+			ends[i] = starts[i+1]-1
+			mids[i] = int((starts[i] + ends[i])/2)
+
+	for i in range(0,12):
+		if i == 0:
+			icevals[i] = dataMap.get('Jan')
+		elif i ==1:
+			icevals[i] = dataMap.get('Feb')
+		elif i ==2:
+			icevals[i] = dataMap.get('Mar')
+		elif i ==3:
+			icevals[i] = dataMap.get('April')
+		elif i ==4:
+			icevals[i] = dataMap.get('May')
+		elif i ==5:
+			icevals[i] = dataMap.get('June')
+		elif i ==6:
+			icevals[i] = dataMap.get('July')
+		elif i ==7:
+			icevals[i] = dataMap.get('Aug')
+		elif i ==8:
+			icevals[i] = dataMap.get('Sep')
+		elif i ==9:
+			icevals[i] = dataMap.get('Oct')
+		elif i ==10:
+			icevals[i] = dataMap.get('Nov')
+		elif i ==11:
+			icevals[i] = dataMap.get('Dec')
+
+
+	days_tointerp = np.zeros([14])
+	ice_tointerp = np.zeros([14])
+	ice_tointerp[0] = (icevals[0]+icevals[11])/2
+	ice_tointerp[13] = (icevals[0]+icevals[11])/2
+	days_tointerp[0] = 1
+	days_tointerp[13] = 365
+	for i in range(1,13):
+		days_tointerp[i] = mids[i-1]
+		ice_tointerp[i] = icevals[i-1]
+
+
+	x = days_tointerp
+	y = ice_tointerp
+	f = interpolate.interp1d(x, y,kind='cubic' )
+	xnew = np.arange(1,366, 1)
+	ynew = f(xnew)   # use interpolation function returned by `interp1d`
+	ice_interp = ynew
+
+	return(ice_interp)
+
+def cloud_load():
+	filelist = 'tcdc.eatm.gauss.2014.nc'
+	with Dataset(filelist) as nc:
+		#print(nc.variables)
+		tcdc=nc.variables['tcdc'][...]
+		lat =nc.variables['lat'][...] 
+		lon = nc.variables['lon'][...]
+		time = nc.variables['time'][...]
+
+	w = tcdc[0,0,0]    
+
+	#dealing with the mask in tcdc - assuming it's a value of 0
+	# le_mask = np.ma.getmask(tcdc)
+	# print(le_mask)
+	# #tcdc[le_mask]=0
+
+	firstday = (time[0]) -1 ;
+	newtime = time - firstday;
+
+	ArcticCircleLat = 66.562778
+	#a circuitous way to find the arctic circle indices
+	LatCircle = np.where(lat> 66.562778)
+	LatCircle = LatCircle[0]
+	latlen = len(LatCircle)
+	#print(latlen)
+	latstart = (LatCircle[0])
+	latend = (LatCircle[latlen-1])
+
+	start_indices = [1,32,60,91,121,152,182,213,244,274,305,335]
+	#print(len(start_indices))
+	end_indices = [32,60,91,121,152,182,213,244,274,305,335,365]
+	for i in range (len(start_indices)):
+	    start = start_indices[i]
+	    end = end_indices[i]
+
+	    index = (newtime>=start)*(newtime<end)
+	    where_index = np.where(index)
+	    if i == 0:
+	        Jan = where_index
+	    if i == 1:
+	        Feb = where_index
+	    if i == 2:
+	        Mar = where_index
+	    if i == 3:
+	        April = where_index
+	    if i == 4:
+	        May = where_index
+	    if i == 5:
+	        June = where_index
+	    if i == 6:
+	        July = where_index
+	    if i == 7:
+	        Aug = where_index
+	    if i == 8:
+	        Sep = where_index
+	    if i == 9:
+	        Oct = where_index
+	    if i == 10:
+	        Nov = where_index
+	    if i == 11:
+	        Dec = where_index
+	        
+	        
+	Cloud_cover = collections.OrderedDict()
+	Cloud_cover['Jan'] = np.mean(tcdc[Jan,latstart:latend,:])
+	Cloud_cover['Feb'] = np.mean(tcdc[Feb,latstart:latend,:])
+	Cloud_cover['Mar'] = np.mean(tcdc[Mar,latstart:latend,:])
+	Cloud_cover['April'] = np.mean(tcdc[April,latstart:latend,:])
+	Cloud_cover['May'] = np.mean(tcdc[May,latstart:latend,:])
+	Cloud_cover['June'] = np.mean(tcdc[June,latstart:latend,:])
+	Cloud_cover['July'] = np.mean(tcdc[July,latstart:latend,:])
+	Cloud_cover['Aug'] = np.mean(tcdc[Aug,latstart:latend,:])
+	Cloud_cover['Sep'] = np.mean(tcdc[Sep,latstart:latend,:])
+	Cloud_cover['Oct'] = np.mean(tcdc[Oct,latstart:latend,:])
+	Cloud_cover['Nov'] = np.mean(tcdc[Nov,latstart:latend,:])
+	Cloud_cover['Dec'] = np.mean(tcdc[Dec,latstart:latend,:])
+
+
+	dataMap = Cloud_cover
+
+ 
+	starts = [1,32,60,91,121,152,182,213,244,274,305,335]
+	ends = np.zeros(len(starts))
+	mids = np.zeros(len(starts))
+	cloudvals = np.zeros(len(starts))
+
+	for i in range(0,12):
+
+		if i == 11:
+			ends[i] = 365 
+			mids[i] = int((starts[i] + ends[i])/2)
+		else:
+			ends[i] = starts[i+1]-1
+			mids[i] = int((starts[i] + ends[i])/2)
+
+	for i in range(0,12):
+		if i == 0:
+			cloudvals[i] = dataMap.get('Jan')
+		elif i ==1:
+			cloudvals[i] = dataMap.get('Feb')
+		elif i ==2:
+			cloudvals[i] = dataMap.get('Mar')
+		elif i ==3:
+			cloudvals[i] = dataMap.get('April')
+		elif i ==4:
+			cloudvals[i] = dataMap.get('May')
+		elif i ==5:
+			cloudvals[i] = dataMap.get('June')
+		elif i ==6:
+			cloudvals[i] = dataMap.get('July')
+		elif i ==7:
+			cloudvals[i] = dataMap.get('Aug')
+		elif i ==8:
+			cloudvals[i] = dataMap.get('Sep')
+		elif i ==9:
+			cloudvals[i] = dataMap.get('Oct')
+		elif i ==10:
+			cloudvals[i] = dataMap.get('Nov')
+		elif i ==11:
+			cloudvals[i] = dataMap.get('Dec')
+
+
+	days_tointerp = np.zeros([14])
+	cloud_tointerp = np.zeros([14])
+	cloud_tointerp[0] = (cloudvals[0]+cloudvals[11])/2
+	cloud_tointerp[13] = (cloudvals[0]+cloudvals[11])/2
+	days_tointerp[0] = 1
+	days_tointerp[13] = 365
+	for i in range(1,13):
+		days_tointerp[i] = mids[i-1]
+		cloud_tointerp[i] = cloudvals[i-1]
+
+
+	x = days_tointerp
+	y = cloud_tointerp
+	f = interpolate.interp1d(x, y,kind='cubic' )
+	xnew = np.arange(1,366, 1)
+	ynew = f(xnew)   # use interpolation function returned by `interp1d`
+	
+	cloud_interp = ynew/100
+
+	return(cloud_interp)
+
+def wind_load():
+	wind_file = 'wspd.sig995.mon.ltm.nc'
+
+	with Dataset(wind_file) as nc:
+	#print(nc.variables)
+		wspd=nc.variables['wspd'][...]
+		lat =nc.variables['lat'][...]
+		lon = nc.variables['lon'][...]
+		time = nc.variables['time'][...]
+
+
+
+	ArcticCircleLat = 66.562778
+	#a circuitous way to find the arctic circle indices
+	LatCircle = np.where(lat> 66.562778)
+	LatCircle = LatCircle[0]
+	latlen = len(LatCircle)
+	latstart = (LatCircle[0])
+	latend = (LatCircle[latlen-1])
+
+	wind_dic = collections.OrderedDict()
+	wind_dic['Jan'] = np.mean(wspd[0,latstart:latend,:])
+	wind_dic['Feb'] = np.mean(wspd[1,latstart:latend,:])
+	wind_dic['Mar'] = np.mean(wspd[2,latstart:latend,:])
+	wind_dic['April'] = np.mean(wspd[3,latstart:latend,:])
+	wind_dic['May'] = np.mean(wspd[4,latstart:latend,:])
+	wind_dic['June'] = np.mean(wspd[5,latstart:latend,:])
+	wind_dic['July'] = np.mean(wspd[6,latstart:latend,:])
+	wind_dic['Aug'] = np.mean(wspd[7,latstart:latend,:])
+	wind_dic['Sep'] = np.mean(wspd[8,latstart:latend,:])
+	wind_dic['Oct'] = np.mean(wspd[9,latstart:latend,:])
+	wind_dic['Nov'] = np.mean(wspd[10,latstart:latend,:])
+	wind_dic['Dec'] = np.mean(wspd[11,latstart:latend,:])
+
+
+	dataMap = wind_dic
+
+	starts = [1,32,60,91,121,152,182,213,244,274,305,335]
+	ends = np.zeros(len(starts))
+	mids = np.zeros(len(starts))
+	windvals = np.zeros(len(starts))
+
+
+
+	for i in range(0,12):
+
+		if i == 11:
+			ends[i] = 365
+			mids[i] = int((starts[i] + ends[i])/2)
+		else:
+			ends[i] = starts[i+1]-1
+			mids[i] = int((starts[i] + ends[i])/2)
+
+		for i in range(0,12):
+			if i == 0:
+				windvals[i] = dataMap.get('Jan')
+			elif i ==1:
+				windvals[i] = dataMap.get('Feb')
+			elif i ==2:
+				windvals[i] = dataMap.get('Mar')
+			elif i ==3:
+				windvals[i] = dataMap.get('April')
+			elif i ==4:
+				windvals[i] = dataMap.get('May')
+			elif i ==5:
+				windvals[i] = dataMap.get('June')
+			elif i ==6:
+				windvals[i] = dataMap.get('July')
+			elif i ==7:
+				windvals[i] = dataMap.get('Aug')
+			elif i ==8:
+				windvals[i] = dataMap.get('Sep')
+			elif i ==9:
+				windvals[i] = dataMap.get('Oct')
+			elif i ==10:
+				windvals[i] = dataMap.get('Nov')
+			elif i ==11:
+				windvals[i] = dataMap.get('Dec')
+
+
+	days_tointerp = np.zeros([14])
+	wind_tointerp = np.zeros([14])
+	wind_tointerp[0] = (windvals[0]+windvals[11])/2
+	wind_tointerp[13] = (windvals[0]+windvals[11])/2
+	days_tointerp[0] = 1
+	days_tointerp[13] = 365
+	for i in range(1,13):
+		days_tointerp[i] = mids[i-1]
+		wind_tointerp[i] = windvals[i-1]
+
+
+	x = days_tointerp
+	y = wind_tointerp
+	f = interpolate.interp1d(x, y,kind='cubic' )
+	xnew = np.arange(1,366, 1)
+	ynew = f(xnew)   # use interpolation function returned by `interp1d`
+	wind_interp = ynew
+	# plt.plot(x, y, 'o', xnew, ynew, '-')
+	# plt.show()
+
+	return(wind_interp)
+
+def temp_load():
+
+#these values are taken from WOA 2009 (as a mat file) 
+	temp_dic = collections.OrderedDict()
+	temp_dic['Jan'] = -1.1878#np.mean(air[0,latstart:latend,:])
+	temp_dic['Feb'] =  -1.0299#np.mean(air[1,latstart:latend,:])
+	temp_dic['Mar'] =    -1.1541#np.mean(air[2,latstart:latend,:])
+	temp_dic['April'] = -1.3006#np.mean(air[3,latstart:latend,:])
+	temp_dic['May'] = -1.1803#np.mean(air[4,latstart:latend,:])
+	temp_dic['June'] = -0.7960 #np.mean(air[5,latstart:latend,:])
+	temp_dic['July'] = 0.0897# np.mean(air[6,latstart:latend,:])
+	temp_dic['Aug'] =  0.0934 #np.mean(air[7,latstart:latend,:])
+	temp_dic['Sep'] = -0.2942#np.mean(air[8,latstart:latend,:])
+	temp_dic['Oct'] = -0.7948#.mean(air[9,latstart:latend,:])
+	temp_dic['Nov'] =    -1.0141# np.mean(air[10,latstart:latend,:])
+	temp_dic['Dec'] = -1.1229  #np.mean(air[11,latstart:latend,:])
+
+
+	dataMap = temp_dic
+
+	starts = [1,32,60,91,121,152,182,213,244,274,305,335]
+	ends = np.zeros(len(starts))
+	mids = np.zeros(len(starts))
+	tempvals = np.zeros(len(starts))
+
+
+
+	for i in range(0,12):
+
+		if i == 11:
+			ends[i] = 365
+			mids[i] = int((starts[i] + ends[i])/2)
+		else:
+			ends[i] = starts[i+1]-1
+			mids[i] = int((starts[i] + ends[i])/2)
+
+	for i in range(0,12):
+		if i == 0:
+			tempvals[i] = dataMap.get('Jan')
+		elif i ==1:
+			tempvals[i] = dataMap.get('Feb')
+		elif i ==2:
+			tempvals[i] = dataMap.get('Mar')
+		elif i ==3:
+			tempvals[i] = dataMap.get('April')
+		elif i ==4:
+			tempvals[i] = dataMap.get('May')
+		elif i ==5:
+			tempvals[i] = dataMap.get('June')
+		elif i ==6:
+			tempvals[i] = dataMap.get('July')
+		elif i ==7:
+			tempvals[i] = dataMap.get('Aug')
+		elif i ==8:
+			tempvals[i] = dataMap.get('Sep')
+		elif i ==9:
+			tempvals[i] = dataMap.get('Oct')
+		elif i ==10:
+			tempvals[i] = dataMap.get('Nov')
+		elif i ==11:
+			tempvals[i] = dataMap.get('Dec')
+
+
+	days_tointerp = np.zeros([14])
+	temp_tointerp = np.zeros([14])
+	temp_tointerp[0] = (tempvals[0]+tempvals[11])/2
+	temp_tointerp[13] = (tempvals[0]+tempvals[11])/2
+	days_tointerp[0] = 1
+	days_tointerp[13] = 365
+	for i in range(1,13):
+		days_tointerp[i] = mids[i-1]
+		temp_tointerp[i] = tempvals[i-1]
+
+
+	x = days_tointerp
+	y = temp_tointerp
+	f = interpolate.interp1d(x, y,kind='cubic' )
+	xnew = np.arange(1,366, 1)
+	ynew = f(xnew)   # use interpolation function returned by `interp1d`
+	temp_interp = ynew
+	# plt.plot(x, y, 'o', xnew, ynew, '-')
+	# plt.show()
+	#temp_interp = 
+	return(temp_interp)
+
+
+def par_load():
+	store_par = np.zeros(12)
+	months = np.arange(1,13, 1)
+	# print(months)
+	# print(store_par)
+	i = 0
+
+	for file in glob.glob("./par/*.nc"):
+		#print(file[12:15])
+
+		with Dataset(file) as nc:
+			#print(nc.variables)
+			par=nc.variables['par'][...]
+			lat=nc.variables['lat'][...]
+			lon=nc.variables['lon'][...]
+			le_mask = np.ma.getmask(par)
+			par[le_mask]=np.NaN
+			ArcticCircleLat = 66.562778
+			#a circuitous way to find the arctic circle indices
+			LatCircle = np.where(lat> 66.562778)
+			LatCircle = LatCircle[0]
+			latlen = len(LatCircle)
+			latstart = (LatCircle[0])
+			latend = (LatCircle[latlen-1])
+
+			par_mean = (np.nanmean(par[latstart:latend,:]))
+			# print(par_mean)
+			if math.isnan(par_mean) == False:
+				store_par[i] = par_mean
+			# print('*')
+			# print(i)
+			i = i+1
+
+
+	starts = [1,32,60,91,121,152,182,213,244,274,305,335]
+	ends = np.zeros(len(starts))
+	mids = np.zeros(len(starts))
+	parvals = store_par
+
+	for i in range(0,12):
+
+		if i == 11:
+			ends[i] = 365 
+			mids[i] = int((starts[i] + ends[i])/2)
+		else:
+			ends[i] = starts[i+1]-1
+			mids[i] = int((starts[i] + ends[i])/2)
+
+	days_tointerp = np.zeros([14])
+	par_tointerp = np.zeros([14])
+	par_tointerp[0] = (parvals[0]+parvals[11])/2
+	par_tointerp[13] = (parvals[0]+parvals[11])/2
+	days_tointerp[0] = 1
+	days_tointerp[13] = 365
+
+	for i in range(1,13):
+		days_tointerp[i] = mids[i-1]
+		par_tointerp[i] = parvals[i-1]
+
+
+	x = days_tointerp
+	y = par_tointerp
+	f = interpolate.interp1d(x, y,kind='cubic' )
+	xnew = np.arange(1,366, 1)
+	ynew = f(xnew)   # use interpolation function returned by `interp1d`
+	par_interp = ynew
+
+	for i in range(0,len(par_interp)):
+		if par_interp[i]<0:
+			par_interp[i] = 0
+
+
+	return(par_interp)
+
+def ccsen_load():
+	x = [1,165,166,365]
+	y = [.9,.2,.2,.9]
+	f = interpolate.interp1d(x, y,kind='cubic' )
+	xnew = np.arange(1,366, 1)
+	ynew = f(xnew)
+	return(ynew)
+
+
+def met_get():
+	met_var = collections.OrderedDict()
+	met_var['ice'] = ice_load()
+	ice = (met_var.get('ice'))
+	icemelt = np.zeros(len(ice))
+	for i in range(1,len(ice)):
+		ice_melt = ice[i-1]-ice[i]
+		if(ice_melt>=0):
+			icemelt[i] = ice_melt
+	met_var['icemelt'] = icemelt
+	met_var['temp'] = temp_load()
+	met_var['wind'] = wind_load()
+	met_var['par'] = par_load()
+	met_var['ccsen'] = ccsen_load()
+	met_var['cloud'] = cloud_load()
+
+	return met_var
+
+wind_file = 'met_var/wspd.sig995.mon.ltm.nc'
+ice_file = 'ice/icec.day.mean.2014.v2.nc'
+
+
+
+met = met_get()
---
+++ PROJEKT/CORE/runmodel.py
@@ -0,0 +1,231 @@
+import INTFUN
+from importlib import reload
+from INTFUN import Integrator
+reload(INTFUN)
+from runmodel_withfeedback import Integ591
+from collections import namedtuple
+from interp_met import met_get, temp_load, ice_load, wind_load
+import numpy as np
+#%matplotlib inline
+import matplotlib.pyplot as plt
+import pandas as pd
+import glob, os
+# import math
+# from importlib import reload
+# reload(INTFUN)
+# from collections import namedtuple
+# import numpy as np
+# #matplotlib inline
+# import matplotlib.pyplot as plt
+import pandas as pd
+import datetime
+
+
+
+class Integ59(Integrator):
+
+    def set_yinit(self):
+
+        initvars = namedtuple('initvars', self.config['initvars'].keys())
+        self.initvars = initvars(**self.config['initvars'])
+        self.yinit = np.array(
+            [self.initvars.primprod,
+             self.initvars.seaDMS,
+             self.initvars.iceDMS,
+             self.initvars.airDMS,
+             self.initvars.airDMS_icecont,
+             self.initvars.CCN,
+             self.initvars.CCN_icecont,
+             self.initvars.forcing,
+             self.initvars.forcing_icecont])
+      
+        self.nvars = len(self.yinit)
+        return None
+
+    def __init__(self, coeff_file_name,met):
+        super().__init__(coeff_file_name,met)
+        self.set_yinit()
+
+
+    def derivs5(self, y, t,met):
+
+        # user = self.uservars
+        ice = met.get('ice')
+        wind = met.get('wind')
+        sst = met.get('sst')
+        par = met.get('par')
+        icemelt = met.get('icemelt')
+        ccsen = met.get('ccsen')
+        cc = met.get('cloud')
+        # print('icemelt')
+        # print(icemelt)
+        parmax = 36.954097122
+        f = np.empty_like(y)
+
+        #equations for primary prod    
+        Tmax = 0.201127738931
+        sst = sst 
+        mu_0 = .79
+        chi = .4
+        albedo = .5
+        #cc = .5
+
+        R_L = ((par)/parmax)*(np.sqrt(1+((par*(1)/parmax)**2)))
+        R_T = np.exp(0.063*(sst-Tmax))
+        mu = mu_0*R_L*R_T 
+
+        #equations for flux
+        Sc = 2674.0 - 147.12*(sst) + 3.726*((sst)**2) - 0.038*((sst)**3)
+        alpha = (600/Sc)**(2/3)
+        beta = (600/Sc)**(1/2)
+
+
+
+        if (wind <= 3.6):
+            k_w = alpha * 0.17 * wind
+        elif (wind <= 13):
+            k_w = beta * (2.85*wind - 10.3) + 0.61 * alpha
+        elif (wind > 13):
+            k_w = beta * (5.9*wind - 49.9) + 0.61 * alpha
+
+        #primprod    
+        f[0] = (y[0]*(mu-chi))*(1-ice)
+        #seaDMS
+        f[1] = (y[1]*(mu-chi))*(1-ice)
+        DMS = y[1]+f[1]
+        # print('DMS')
+        # print(DMS)
+        #iceDMS
+        iceDMS = icemelt * 100
+        # print('iceDMS')
+        #print(iceDMS)
+        f[2] = iceDMS - y[2]
+        #airDMS
+
+        #DMS_copy = DMS
+        DMSFLUX = k_w*DMS
+        f[3] = DMSFLUX-y[3]
+        totDMS = DMS+iceDMS
+        #print(totDMS)
+        #airDMS_icecont
+        DMSFLUX_icecont = k_w*(totDMS)
+        f[4] =  DMSFLUX_icecont - y[4]
+        #CCN
+        perc_change = ccsen*((DMSFLUX-y[3])/y[3])
+        CCN = (1+perc_change)*y[5]
+
+        f[5] = CCN - y[5]
+        delt_N = f[5]/CCN
+        #CCN icecont
+        perc_change = ccsen*((DMSFLUX_icecont-y[4])/y[4])
+        CCN_icecont = (1+perc_change)*y[6]
+        f[6] = CCN_icecont - y[6]
+        delt_N_ice = f[6]/CCN_icecont
+        #forcing
+        f[7] = (-1/3)*par*cc*albedo*(1-albedo)*delt_N
+        #forcing_icecont
+        f[8] = (-1/3)*par*cc*albedo*(1-albedo)*delt_N_ice
+        
+        return f  
+
+
+met = met_get()
+theSolver = Integ59('test.yaml',met)
+timeVals,yVals,errorList,metList=theSolver.timeloop5fixed(met)
+yvals=pd.DataFrame.from_records(yVals,columns=['primprod','sea DMS','ice DMS','air DMS','air_dms_icecont','CCN', 'CCN_icecont', 'forcing', 'forcing_icecont'])
+
+plt.close("all")
+
+fig, big_axes = plt.subplots( figsize=(10.0, 10.0) , nrows=3, ncols=1, sharey=True) 
+for row, big_ax in enumerate(big_axes, start=1):
+    big_ax.tick_params(labelcolor=(1.,1.,1., 0.0), top='off', bottom='off', left='off', right='off')
+    # removes the white frame
+    big_ax._frameon = False
+
+yvals=pd.DataFrame.from_records(yVals,columns=['primprod','sea DMS','ice DMS','air DMS','air_dms_icecont','CCN', 'CCN_icecont', 'forcing', 'forcing_icecont'])
+
+str_list = ['primprod','sea DMS','ice DMS','air DMS','air_dms_icecont','CCN', 'CCN_icecont', 'forcing', 'forcing_icecont']
+var_list = ['primprod','sea DMS','ice DMS','air DMS','air_dms_icecont','CCN', 'CCN_icecont', 'forcing', 'forcing_icecont']
+key_list = ['primprod','sea DMS','ice DMS','air DMS','air_dms_icecont','CCN', 'CCN_icecont', 'forcing', 'forcing_icecont']
+
+for i in range(1,10):
+    
+    ax = fig.add_subplot(3,3,i)
+
+    theLines = ax.plot(timeVals,yvals[var_list[i-1]],'b*',label=key_list[i-1])
+    theLines[0].set_marker('+')
+    theLines[0].set_linestyle('-')
+
+    titl =  str_list[i-1]  
+    ax.hold(True)
+    ax.set_title(titl, fontsize = 10)
+    ax.set_xlabel("time (day)", fontsize = 8)
+
+    ax.set_xlim([0,350])
+
+fig.tight_layout()
+fig.set_facecolor('w')
+plt.suptitle('Model Variables', fontsize=16, fontweight='bold')
+w = datetime.datetime.now()
+w.isoformat()
+day = str(w)
+filename = './model_output/Model_output' + day + '.png'
+fig.savefig(filename)
+# print(yvals['DMS flux'])
+
+days = np.arange(1,366,1)
+ice = np.array(metList.get('ice'))
+wind = np.array(metList.get('wind'))
+sst = np.array(metList.get('sst'))
+par = np.array(metList.get('par'))
+ccsen = np.array(metList.get('ccsen'))
+cloud = np.array(metList.get('cloud'))
+print('days')
+print(len(days))
+print('ice')
+print(len(ice))
+
+
+fig2, big_axes = plt.subplots( figsize=(10.0, 10.0) , nrows=3, ncols=1, sharey=True) 
+for row, big_ax in enumerate(big_axes, start=1):
+    big_ax.tick_params(labelcolor=(1.,1.,1., 0.0), top='off', bottom='off', left='off', right='off')
+    # removes the white frame
+    big_ax._frameon = False
+
+met_list = [ice,wind,sst,par,ccsen,cloud]
+tit_list = ['Ice Cover','Windspeed','Sea Surface Temperature','Light Intensity','CCN sensitivity to DMS flux', 'Cloud Cover']
+ylabel_list = []
+for i in range(1,7):
+    
+    ax = fig2.add_subplot(3,2,i)
+
+    theLines = ax.plot(days,met_list[i-1],'b*')
+# points.set_markersize(12) 
+    theLines[0].set_marker('+')
+    theLines[0].set_linestyle('-')
+
+    titl =  'Climatology of ' + tit_list[i-1]
+    ax.hold(True)
+    ax.set_title(titl, fontsize = 12)
+    ax.set_xlabel("time (day)", fontsize = 8)
+
+    ax.set_xlim([0,365])
+
+    if (i ==1):
+        ax.set_ylabel("fractional coverage")
+    if (i ==2):
+        ax.set_ylabel("m/s")
+    if (i ==3):
+        ax.set_ylabel("degrees Celsius")
+    if (i ==4):
+        ax.set_ylabel("W /m^2")
+fig2.set_facecolor('w')
+fig2.tight_layout()
+#plt.suptitle('Meteorological Forcing', fontsize=12, fontweight='bold')
+w = datetime.datetime.now()
+w.isoformat()
+print(w)
+day = str(w)
+filename = './model_output/met_force' + day + '.png'
+fig2.savefig(filename)
+plt.show()
